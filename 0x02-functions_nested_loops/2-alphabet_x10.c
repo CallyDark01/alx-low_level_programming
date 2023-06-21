bee3d59d9ca601a055df27211e09f36845987a56
CallyDark01 <calebdarkwa@gmail.com>
2023-06-21 15:30:36 -0700

    2-alphabet_x10.c

--- 0x02-functions_nested_loops/2-alphabet_x10.c
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -7,16 +7,21 @@
  */
 
 void print_alphabet_x10(void)
+int _putchar(char c)
+
 {
 	char c;
 	int i;
+
 	i = 0;
 	while (i < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; ch++)
 		{
-			_putchar("%c ", c);
+			_putchar(c);
 		}
 		_putchar('\n');
+		i++;
 	}
+	return ();
 }
